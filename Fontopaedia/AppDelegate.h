347e8471dc0dc3d15592543227faa00d0c197013
Yosamac <yosamac@gmail.com>
2015-02-07 16:41:46 +0100

    Initial Commit

---
+++ Fontopaedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fontopaedia
+//
+//  Created by Yosnier on 07/02/15.
+//  Copyright (c) 2015 YOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
